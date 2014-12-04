d6c3e23b77ef843e22cae9a6fed155936f57d917
domitry <domitry@gmail.com>
2014-12-04 23:43:29 +0900

    Modify so

--- Rakefile
+++ Rakefile
@@ -7,6 +7,6 @@ Bundler.setup(:default, :development)
 Rake::ExtensionTask.new do |ext|
   ext.name = 'ode'
   ext.ext_dir = 'ext/ode'
-  ext.lib_dir = 'lib/'
+  ext.lib_dir = 'lib/so'
   ext.source_pattern = "**/*.{f, c, h}"
 end
--- lib/ode.rb
+++ lib/ode.rb
@@ -1,9 +1,11 @@
 require_relative "ode/version"
 require_relative "ode/solver"
 require_relative "ode/methods"
-require_relative "ode.so"
+require_relative "so/ode.so"
 
 module Ode
   module Methods
   end
 end
+
+print "Hello, world!"
